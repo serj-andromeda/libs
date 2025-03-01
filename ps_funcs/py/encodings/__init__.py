144110df2450e1a488c7d180b1f517a11cb7bd93
Serj Andromeda Honor <183820079+serj-andromeda@users.noreply.github.com>
2025-03-02 01:22:00 +0300

    auto-commit after backup of '_bashrc' without explicit params. Backup target is auto-detected

--- ps_funcs/py/encodings/__init__.py
+++ ps_funcs/py/encodings/__init__.py
@@ -1,3 +1,14 @@
 """
 this sub-package includes modules for various encodings. See list below in `PACKAGE CONTENTS` section.
 """
+
+
+
+
+
+
+from . import unicode, ascii
+__all__=[
+unicode,
+ascii,
+]
--- ps_funcs/py/encodings/unicode.py
+++ ps_funcs/py/encodings/unicode.py
@@ -1,3 +1,36 @@
 chars={
 'Horizontal Ellipsis':'…',
 }
+
+
+
+def get_hex_chars_list ()->list:
+	return list(map (str,range(10)))+[chr(c) for c in range(ord('a'), ord('g'))]+[chr(c) for c in range(ord('A'), ord('G'))]
+
+
+
+def print_char_from_hexcode (code_str)->bool:
+	"""
+		Convert hex=code to Unicode char
+		Cut non-hexades chars from beginning
+	"""
+	hex_chars_list=get_hex_chars_list()
+	while code_str[0] not in hex_chars_list:
+		code_str=code_str[1:]
+	print (f"@ps list of hex chars and str after clean: {hex_chars_list} {code_str}")
+	char_str=get_char_from_hexcode (code_str)
+	print (char_str)
+
+
+
+
+
+
+
+
+def get_char_from_hexcode (code_str)->str:
+	code_int=int(code_str, 16)
+	return chr (code_int)
+
+
+
