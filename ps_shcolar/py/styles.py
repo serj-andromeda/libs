e5d38972cb692b416e21bcbf2f420c088d13fb8d
Serj Andromeda Phone <183820079+serj-andromeda@users.noreply.github.com>
2025-04-04 21:56:19 +0300

    few sryles

--- ps_shcolar/py/styles.py
+++ ps_shcolar/py/styles.py
@@ -10,29 +10,13 @@ dim="\033[2m";
 """Escape sequence for dimmed text"""
 italic="\033[3m";
 """Escape sequence for italic text"""
+
 underline="\033[4m";
 """Escape sequence for underlined text"""
-blue="\033[44m";
-"""Escape sequence for background blue color"""
-magenta="\033[45m";
-"""Escape sequence for background magenta color"""
-cyan="\033[46m";
-"""Escape sequence for background cyan color"""
-white="\033[47m";
-"""Escape sequence for background white color"""
-gray="\033[100m";
-"""Escape sequence for background gray color"""
-brightRed="\033[101m";
-"""Escape sequence for background bright red color"""
-brightGreen="\033[102m";
-"""Escape sequence for background bright green color"""
-brightYellow="\033[103m";
-"""Escape sequence for background bright yellow color"""
-brightBlue="\033[104m";
-"""Escape sequence for background bright blue color"""
-brightMagenta="\033[105m";
-"""Escape sequence for background bright magenta color"""
-brightCyan="\033[106m";
-"""Escape sequence for background bright cyan color"""
-brightWhite="\033[107m";
-"""Escape sequence for background bright white color"""
+
+invert="\033[7m";
+"""Escape sequence for inverted text"""
+
+underline="\033[4m";
+"""Escape sequence for underlined text"""
+
