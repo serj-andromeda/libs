248becefaf1597a20b01f47093b4af393b58818e
Serj Andromeda Phone <183820079+serj-andromeda@users.noreply.github.com>
2025-07-25 17:24:20 +0300

    logs

--- ps_funcs/py/log.py
+++ ps_funcs/py/log.py
@@ -4,3 +4,17 @@ Most of logging parameters configurable through function arguments.
 """
 import os, sys; import ps_funcs, ps_shcolar
 
+class PSLog:
+	def set_fn_tpl (self, fn_tpl:str):
+		self._fn_tpl=fn_tpl
+	def set_dn_tpl (self, dn_tpl:str):
+		self._dn_tpl=dn_tpl
+	def set_line_tpl (self, line_tpl:str):
+		self._line_tpl=line_tpl
+	def set_tag (self, tag_str:str):
+		self._tag_str=tag_str
+	def __init_ (self, log_dn_tpl: str, tag_str: str, line_tpl: str):
+		self.set_dn_tpl (log_dn_tpl)
+		self.set_fn_tpl (log_fn_tpl)
+		self.set_line_tpl (line_tpl)
+	def log (self, msg)
